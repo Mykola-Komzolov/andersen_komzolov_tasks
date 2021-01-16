c32c4a4972bc6957b9bf610fc335cd5ff5ed3263
Николай Комзолов <77415282+Mykola-Komzolov@users.noreply.github.com>
2021-01-17 00:45:11 +0200

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,37 @@
+print ('Введите скобочную последовательность:')
+x = input()
+x = str(x)
+K = 0
+K = int(K)
+C = 0
+c = int(C)
+Ch = 0
+Ch = int(Ch)
+g = 1
+g = int (g)
+for i in range (len(x)):
+    if x[i] == '[':
+        K = K + 1
+    elif x[i] == ']':
+        K = K - 1
+    elif x[i] == '(':
+        C = C + 1
+    elif x[i] == ')':
+        C = C - 1
+    if K < 0:
+        K = K + 1
+        g = 0
+        print ('Нет открывающейся скобки для ]')
+    elif C < 0:
+        C = C + 1
+        g = 0
+        print ('Нет открывающейся скобки для )')
+if (K > 0):
+    g = 0
+    print ("Не закрытая скобка [")
+elif (C > 0):
+    g = 0
+    print ("Не закрытая скобка (")
+if g == 1:
+    print ('Всё верно.')
+input ('Press Enter to exit')
